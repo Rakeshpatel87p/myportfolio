1c11f18cfea161a95f687616dd6bbe0670a09cb8
Rakesh Patel <emailme@rakeshrpatel.com>
2016-03-28 15:37:02 +0530

    smooth scroll for smooth crim

--- js/script.js
+++ js/script.js
@@ -1,41 +1,36 @@
 $(document).ready(function() {
 
-        // $(".information" a).click(function() {
-        //     var id = $(this).attr('href');
-        //     $("html, body").animate({ scrollTop: $(id).offset().top()}, 2000);
-        //     return false;
-      // });
+    $(".smooth_scroll").click(function() {
+        $("html, body").animate({ //takes entire html, body and animates
+            'scrollTop':$(this).offset().top 
+        }, 700);
+//We get the top coordinate of the cicked item and tell the webpage via
+// the scrollbar to move down to that position at the defined pace.
+    });
+
 
     $("#aboutme_link").click(function() {
-        $(".aboutme").show();
+        $(".aboutme").toggle();
         $(".aboutme").siblings().hide();
-        // $("#aboutme_link").addClass("styling_links")
-        // $("#aboutme_link").siblings().removeClass("styling_links");
-        /*$(".portfolio").hide();*/
         $(this).siblings().removeClass("styling_links2").removeClass("styling_links3");
         $(this).addClass("styling_links");
-        
+
 
     })
 
     $("#portfolio_link").click(function() {
-        $(".portfolio").show();
+        $(".portfolio").toggle();
         $(".portfolio").siblings().hide();
         $(this).addClass("styling_links2");
-        // $(".aboutme").hide();
-        // $(".contact").hide();
         $(this).siblings().removeClass("styling_links").removeClass("styling_links3");
     })
 
     $("#contact_link").click(function() {
-        $(".contact").show();
+        $(".contact").toggle();
         $(".contact").siblings().hide();
         $(this).addClass("styling_links3");
-        // $(".portfolio").hide();
-        // $(".aboutme").hide();
         $(this).siblings().removeClass("styling_links").removeClass("styling_links2");
 
     })
 
 });
-
