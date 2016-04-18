4b76508aa710ae71677fca26de813345d1d4ab76
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-18 13:00:44 +0530

    font-awesome fonts added

--- js/script.js
+++ js/script.js
@@ -1,36 +1,47 @@
-$(document).ready(function() {
+var headers = $(".headers > h2").toArray();
+var images = $(".portfolio-images > img").toArray();
+var i = 0;
 
-    $(".smooth_scroll").click(function() {
-        $("html, body").animate({ //takes entire html, body and animates
-            'scrollTop':$(this).offset().top 
-        }, 700);
-//We get the top coordinate of the cicked item and tell the webpage via
-// the scrollbar to move down to that position at the defined pace.
+$(function() {
+
+    $('.name').click(function(){
+        $('.aboutme').slideToggle(1000);
+        $('.masthead').toggleClass('masthead-change');
+
     });
 
+    $('.jobtitle').click(function(){
+    	$('.masthead').toggleClass('masthead-change');
+        $('.aboutme').hide();
+        fill(i);
+    	console.log();
 
-    $("#aboutme_link").click(function() {
-        $(".aboutme").toggle();
-        $(".aboutme").siblings().hide();
-        $(this).siblings().removeClass("styling_links2").removeClass("styling_links3");
-        $(this).addClass("styling_links");
-
-
-    })
-
-    $("#portfolio_link").click(function() {
-        $(".portfolio").toggle();
-        $(".portfolio").siblings().hide();
-        $(this).addClass("styling_links2");
-        $(this).siblings().removeClass("styling_links").removeClass("styling_links3");
-    })
-
-    $("#contact_link").click(function() {
-        $(".contact").toggle();
-        $(".contact").siblings().hide();
-        $(this).addClass("styling_links3");
-        $(this).siblings().removeClass("styling_links").removeClass("styling_links2");
+    	$(".navigateForward").click(function() {
+        if (i < 3) {
+            i++;
+            fill(i);
+        } else if (i == 3) {
+        	i=0;
+        	fill(i);
+        };
 
     })
 
-});
+})    	
+
+});    
+
+function pulsate() {
+  for(var i=0; i<2; i++) {
+    $("#pulsate").animate({opacity: 0.2}, 1000, 'linear')
+     .animate({opacity: 1}, 1000, 'linear');
+     console.log()
+  }
+}
+
+function fill(i) {
+        return $(headers[i]).slideToggle().siblings().hide(),
+            $(images[i]).slideToggle(1000).siblings().hide(),
+            console.log(i);
+    }
+
